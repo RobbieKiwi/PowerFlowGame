e107dae6141aac24fb8e4634a0ed47db97833fe4
Robbie Muir <robbie.muir@gmail.com>
2025-05-03 11:30:16 +0200

    improved game state

--- src/models/game_state.py
+++ src/models/game_state.py
@@ -1,18 +1,72 @@
 from dataclasses import dataclass
-from typing import Self
+from enum import Enum
+from functools import cached_property
+from typing import Self, Optional
 import json
 
+from src.models.player import Player, PlayerId
 
-@dataclass(frozen=True)
+
+class Phase(Enum):
+    # Values are just placeholders
+    CONSTRUCTION = 0
+    SNEAKY_TRICKS = 1
+    DA_AUCTION = 2
+
+
+@dataclass
 class GameState:
     # A complete description of the current state of the game.
-    game_id: int
-    # TODO Fill in more stuff
+    def __init__(
+        self,
+        game_id: int,
+        players: list[Player],
+        phase: Phase,
+        current_player: Optional[PlayerId],
+    ) -> None:
+        # Read only attributes
+        self._game_id = game_id
+        self._players = players
 
-    def to_json(self) -> str:
-        return json.dumps({"game_id": self.game_id})
+        # Mutable attributes
+        self.phase = phase
+        self.current_player = current_player
+
+    @property
+    def game_id(self) -> int:
+        return self._game_id
+
+    @property
+    def players(self) -> list[Player]:
+        return self._players
+
+    @cached_property
+    def n_players(self) -> int:
+        return len(self.players)
+
+    def to_simple_dict(self) -> dict:
+        return {
+            "game_id": self.game_id,
+            "players": [player.to_simple_dict() for player in self.players],
+            "phase": self.phase.value,
+            "current_player": (
+                self.current_player.as_int()
+                if self.current_player is not None
+                else None
+            ),
+        }
 
     @classmethod
-    def from_json(cls, json_str: str) -> Self:
-        json_dict = json.loads(json_str)
-        return cls(**json_dict)
+    def from_simple_dict(cls, simple_dict: dict) -> Self:
+        return cls(
+            game_id=simple_dict["game_id"],
+            players=[
+                Player.from_simple_dict(player) for player in simple_dict["players"]
+            ],
+            phase=Phase(simple_dict["phase"]),
+            current_player=(
+                PlayerId(simple_dict["current_player"])
+                if simple_dict["current_player"] is not None
+                else None
+            ),
+        )
