e756da381328617ab9f32d49098972d56865193e
RomanCantu <roman94.cr@gmail.com>
2025-05-24 23:13:32 +0200

    add GameSettings to GameState

--- src/models/game_state.py
+++ src/models/game_state.py
@@ -4,10 +4,14 @@ from typing import Self, Optional
 
 from src.models.assets import AssetRepo
 from src.models.buses import BusRepo
-from src.models.ids import PlayerId
+from src.models.ids import (
+    PlayerId,
+    GameId,
+)
 from src.models.market_coupling_result import MarketCouplingResult
 from src.models.player import PlayerRepo
 from src.models.transmission import TransmissionRepo
+from src.models.game_settings import GameSettings
 from src.tools.serialization import (
     simplify_type,
     un_simplify_type,
@@ -24,7 +28,8 @@ class Phase(IntEnum):
 @dataclass
 class GameState:
     # A complete description of the current state of the game.
-    game_id: int
+    game_id: GameId
+    game_settings: GameSettings
     phase: Phase
     players: PlayerRepo
     buses: BusRepo
@@ -39,6 +44,7 @@ class GameState:
     def to_simple_dict(self) -> dict:
         return {
             "game_id": self.game_id,
+            "game_settings": self.game_settings.to_simple_dict(),
             "phase": simplify_type(self.phase),
             "players": self.players.to_simple_dict(),
             "buses": self.buses.to_simple_dict(),
@@ -51,6 +57,7 @@ class GameState:
     def from_simple_dict(cls, simple_dict: dict) -> Self:
         return cls(
             game_id=simple_dict["game_id"],
+            game_settings=GameSettings.from_simple_dict(simple_dict["game_settings"]),
             phase=un_simplify_type(x=simple_dict["phase"], t=Phase),
             players=PlayerRepo.from_simple_dict(simple_dict["players"]),
             buses=BusRepo.from_simple_dict(simple_dict["buses"]),
