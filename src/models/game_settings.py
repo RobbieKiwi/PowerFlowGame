d95048802483774e7b2e738e56c57e1a9220ff34
Roman Cantu <58509779+RomanCantu@users.noreply.github.com>
2025-05-25 22:50:12 +0200

    Feature/new game (#6)
    * add GameId
    
    * suggested: remove UI attributes from here
    
    * rename Rules to GameSettings
    
    * add GameSettings to GameState
    
    * removed Rules
    
    * create new game
    
    * revision PR
    
    ---------
    
    Co-authored-by: RomanCantu <roman94.cr@gmail.com>

---
+++ src/models/game_settings.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import Self
+
+
+@dataclass(frozen=True)
+class GameSettings:
+    """A class to hold game settings."""
+    n_buses: int = 5
+    max_rounds: int = 20
+    n_ice_cream: int = 5
+    initial_funds: int = 1000
+    max_connections_per_bus: int = 7
+
+    def to_simple_dict(self) -> dict:
+        """Convert the game settings to a simple dictionary."""
+        return {
+            "n_buses": self.n_buses,
+            "max_rounds": self.max_rounds,
+            "n_ice_cream": self.n_ice_cream,
+            "initial_funds": self.initial_funds,
+            "max_connections_per_bus": self.max_connections_per_bus,
+        }
+
+    @classmethod
+    def from_simple_dict(cls, simple_dict: dict) -> Self:
+        """Create a GameSettings instance from a simple dictionary."""
+        return cls(
+            n_buses=simple_dict["n_buses"],
+            max_rounds=simple_dict["max_rounds"],
+            n_ice_cream=simple_dict["n_ice_cream"],
+            initial_funds=simple_dict["initial_funds"],
+            max_connections_per_bus=simple_dict["max_connections_per_bus"],
+        )
+
