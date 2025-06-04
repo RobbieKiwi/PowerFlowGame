0a93244f1ac5c1c45bad65627c7cbdaa0f914a61
Robbie <robbiemuir7@gmail.com>
2025-06-04 21:17:20 +0200

    Added some boiler plate code for events (#8)

--- src/engine/market_coupling.py
+++ src/engine/market_coupling.py
@@ -1,15 +1,15 @@
 from src.models.assets import AssetRepo
+from src.models.game_state import GameState
 from src.models.market_coupling_result import MarketCouplingResult
 from src.models.transmission import TransmissionRepo
 
 
 class MarketCouplingCalculator:
     @classmethod
-    def run(cls, assets: AssetRepo, transmission: TransmissionRepo) -> MarketCouplingResult:
+    def run(cls, game_state: GameState) -> MarketCouplingResult:
         """
         Run the market coupling algorithm.
-        :param assets: The asset repository
-        :param transmission: The transmission repository
+        :param game_state: A copy of the game state
         :return: The market coupling result
         """
         # TODO Implement the market coupling algorithm
