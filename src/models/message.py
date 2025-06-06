0f0d47ed0c6411443f7b8c17e8eb078faae1845f
Robbie Muir <robbiemuir7@gmail.com>
2025-06-06 12:09:07 +0200

    some refactoring

--- src/models/message.py
+++ src/models/message.py
@@ -1,76 +1,89 @@
 from abc import ABC
 from dataclasses import dataclass
+from typing import Union
 
 from src.models.game_state import GameState, Phase
 from src.models.ids import PlayerId, AssetId
 
 
 @dataclass(frozen=True)
-class Event(ABC):
+class Message(ABC):
     pass
 
 
 @dataclass(frozen=True)
-class NewPhase(Event):
-    phase: Phase
-
-
-@dataclass(frozen=True)
-class PlayerEvent(Event, ABC):
-    # An event triggered by a player that is sent to the game engine
-    player_id: PlayerId
-
+class InternalMessage(Message, ABC):
+    # A message from the game to itself
     def __str__(self) -> str:
-        return f"<{self.__class__.__name__}({self.player_id} -> Game Engine)>"
+        return f"<{self.__class__.__name__}>"
 
     def __repr__(self) -> str:
         return str(self)
 
 
 @dataclass(frozen=True)
-class EngineEvent(Event, ABC):
-    # An event triggered by the game engine that is sent to the player
+class PlayerToGameMessage(Message, ABC):
+    player_id: PlayerId
+
+    def __str__(self) -> str:
+        return f"<{self.__class__.__name__}({self.player_id} -> Game)>"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+@dataclass(frozen=True)
+class GameToPlayerMessage(Message, ABC):
     player_id: PlayerId
     game_state: GameState
 
     def __str__(self) -> str:
-        return f"<{self.__class__.__name__}(Game Engine -> {self.player_id})>"
+        return f"<{self.__class__.__name__}(Engine -> {self.player_id})>"
 
     def __repr__(self) -> str:
         return str(self)
 
 
+ToGameMessage = Union[PlayerToGameMessage, InternalMessage]
+FromGameMessage = Union[InternalMessage, GameToPlayerMessage]
+
+
 @dataclass(frozen=True)
-class GameUpdate(EngineEvent):
+class NewPhase(InternalMessage):
+    phase: Phase
+
+
+@dataclass(frozen=True)
+class GameUpdate(GameToPlayerMessage):
     # The basic message that gets sent to a player to let them know the game state has
     pass
 
 
 @dataclass(frozen=True)
-class UpdateBidRequest(PlayerEvent):
+class UpdateBidRequest(PlayerToGameMessage):
     asset_id: AssetId
     bid_price: float
 
 
 @dataclass(frozen=True)
-class UpdateBidResponse(EngineEvent):
+class UpdateBidResponse(GameToPlayerMessage):
     game_state: GameState
     success: bool
     asset_id: AssetId
 
 
 @dataclass(frozen=True)
-class BuyAssetRequest(PlayerEvent):
+class BuyAssetRequest(PlayerToGameMessage):
     asset_id: AssetId
 
 
 @dataclass(frozen=True)
-class BuyAssetResponse(EngineEvent):
+class BuyAssetResponse(GameToPlayerMessage):
     game_state: GameState
     success: bool
     asset_id: AssetId
 
 
 @dataclass(frozen=True)
-class EndTurn(PlayerEvent):
+class EndTurn(PlayerToGameMessage):
     asset_id: AssetId
